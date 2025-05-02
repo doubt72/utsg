587558670ba95478100464d149989aa5885e3c47
Douglas Triggs <douglas@triggs.org>
2025-05-02 02:16:33 -0600

    Fix reinforcement control overlay

--- app/javascript/components/game/controls/Reinforcements.tsx
+++ app/javascript/components/game/controls/Reinforcements.tsx
@@ -4,6 +4,25 @@ import Map from "../../../engine/Map";
 import { Player } from "../../../utilities/commonTypes";
 import { gamePhaseType } from "../../../engine/Game";
 
+interface OverlayProps {
+  x: number;
+  y: number;
+  player: Player;
+  fillColor: string;
+  callback: (x: number, y: number, player: Player) => void;
+  mouseIn: () => void;
+  mouseOut: () => void;
+}
+
+function Overlay({ x, y, player, callback, mouseIn, mouseOut, fillColor}: OverlayProps) {
+  return (
+    <path d={baseCounterPath(x, y)} style={{ fill: fillColor }}
+          onClick={() => callback(x, y, player)}
+          onMouseEnter={() => mouseIn()}
+          onMouseLeave={() => mouseOut()} />
+  )
+}
+
 interface ReinforcementsProps {
   map: Map;
   xx: number;
@@ -14,35 +33,49 @@ interface ReinforcementsProps {
 
 export default function Reinforcements({ map, xx, yy, callback, update }: ReinforcementsProps) {
   const [base, setBase] = useState<JSX.Element | undefined>()
-  const [mouseover, setMouseover] = useState<JSX.Element | undefined>()
+  const [hover, setHover] = useState<JSX.Element | undefined>()
 
-  const enter = (x: number, y: number) => {
-    console.log(`entering ${x} ${y}`)
-    setMouseover(
-      <path d={baseCounterPath(x, y)} style={{ fill: "rgba(1,0,0,0.5)" }}/>
-    )
+  const enter = () => {
+    setHover(h => {
+      if (h) {
+        const x = h.props.x
+        const y = h.props.y
+        const player = h.props.player
+        return <Overlay x={x} y={y} player={player} callback={callback} mouseIn={enter} mouseOut={exit}
+                        fillColor="rgba(255,255,255,0.1)" />
+      }
+      return h
+    })
+  }
+
+  const exit = () => {
+    setHover(h => {
+      if (h) {
+        const x = h.props.x
+        const y = h.props.y
+        const player = h.props.player
+        return <Overlay x={x} y={y} player={player} callback={callback} mouseIn={enter} mouseOut={exit}
+                        fillColor="rgba(0,0,0,0)" />
+      }
+      return h
+    })
   }
 
   const nation = (x: number, y: number, n: string, player: Player, enabled: boolean) => {
-    const showDisabled = enabled ? (
-      <path d={baseCounterPath(x, y)} style={{ fill: "rgba(0,0,0,0)" }}
-            onClick={() => callback(x, y, player)}
-            onMouseEnter={() => enter(x, y)}
-            onMouseLeave={() => {
-              console.log("leave")
-              setMouseover(undefined)
-            }} />
+    const overlay = enabled ? (
+      <Overlay x={x} y={y} player={player} callback={callback} mouseIn={enter} mouseOut={exit}
+               fillColor="rgba(0,0,0,0)" />
     ) : (
       <path d={baseCounterPath(x, y)} style={{ fill: "rgba(0,0,0,0.33)" }}
             onClick={() => callback(x, y, player)}/>
     )
+    if (enabled) { setHover(() => overlay) }
     return (
       <g>
         <path d={baseCounterPath(x, y)}
               style={{ fill: nationalColors[n], stroke: "black", strokeWidth: 1 }}/>
         <image width={80} height={80} x={x} y={y} href={`/assets/units/${n}.svg`}/>
-        {mouseover}
-        {showDisabled}
+        {enabled ? undefined : overlay}
       </g>
     )
   }
@@ -101,6 +134,7 @@ export default function Reinforcements({ map, xx, yy, callback, update }: Reinfo
   return (
     <g>
       {base}
+      {hover}
     </g>
   )
 }
